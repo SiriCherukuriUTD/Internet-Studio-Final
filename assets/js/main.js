3fa62053ff5896bd9f541e480f46053086b7b87f
SiriCherukuriUTD <sxc230129@utdallas.edu>
2026-04-30 07:23:02 +0000

    background title

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,17 @@
 // main.js
+console.log("Hello from main.js");
 
-console.log("Hello, World!");
+let targetElement = document.querySelector("h1");
+
+console.log(targetElement.outerHTML);
+
+targetElement.addEventListener("mouseover", function () {
+	console.log(this.innerText);
+	this.innerText = "NO SPACE";
+	this.style = "cursor: wait";
+});
+
+targetElement.addEventListener("mouseout", function () {
+	this.innerText = "MY SPACE";
+	this.style = "cursor: pointer";
+});
